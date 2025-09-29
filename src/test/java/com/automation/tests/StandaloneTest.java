bb65335e3042c3e5a0b9e77561396a7fa065702c
ajaajahmad <ajaaj75@gmail.com>
2025-09-29 12:39:51 +0530

    Standalone Test

--- src/test/java/com/automation/tests/StandaloneTest.java
+++ src/test/java/com/automation/tests/StandaloneTest.java
@@ -1,5 +1,20 @@
 package com.automation.tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class StandaloneTest {
 
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://rahulshettyacademy.com/client/#/auth/login");
+	}
 }
