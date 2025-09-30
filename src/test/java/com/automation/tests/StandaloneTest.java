73970a10e77ae43c42839f70bee98d5a9b849d24
ajaajahmad <ajaaj75@gmail.com>
2025-09-30 20:52:43 +0530

    Standalone Test

--- src/test/java/com/automation/tests/StandaloneTest.java
+++ src/test/java/com/automation/tests/StandaloneTest.java
@@ -1,9 +1,11 @@
 package com.automation.tests;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -20,6 +22,10 @@ public class StandaloneTest {
 		driver.findElement(By.id("userEmail")).sendKeys("test.user@domain.com");
 		driver.findElement(By.id("userPassword")).sendKeys("Asdf@123");
 		driver.findElement(By.id("login")).click();
+
+		List<WebElement> productList = driver.findElements(By.cssSelector(".mb-3"));
+
+		productList.stream().filter(product -> product.getText().equals("ZARA COAT 3"));
 
 	}
 }
