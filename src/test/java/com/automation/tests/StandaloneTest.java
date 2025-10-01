a10939931d8e3360cc56508597640d25db177a98
ajaajahmad <ajaaj75@gmail.com>
2025-10-01 12:28:17 +0530

    Standalone Test

--- src/test/java/com/automation/tests/StandaloneTest.java
+++ src/test/java/com/automation/tests/StandaloneTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -30,6 +31,8 @@ public class StandaloneTest {
 				.orElse(null);
 
 		prod.findElement(By.cssSelector(".card-body  button:last-of-type")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
 
 	}
 }
