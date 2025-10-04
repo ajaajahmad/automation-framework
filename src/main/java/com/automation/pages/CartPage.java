a9667b4ebaf01c9046ec0af5d6c9ab8d1a92b1d7
ajaajahmad <ajaaj75@gmail.com>
2025-10-04 23:00:01 +0530

    Page Factory

---
+++ src/main/java/com/automation/pages/CartPage.java
@@ -0,0 +1,34 @@
+package com.automation.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.automation.base.BasePage;
+
+public class CartPage extends BasePage {
+
+	WebDriver driver;
+
+	public CartPage(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(css = ".cartSection h3")
+	List<WebElement> cartProducts;
+
+	@FindBy(css = ".totalRow button")
+	WebElement checkoutButton;
+
+	public boolean verifyProducDisplay(String productName) {
+
+		boolean match = cartProducts.stream().anyMatch(p -> p.getText().equalsIgnoreCase(productName));
+		return match;
+	}
+
+}
