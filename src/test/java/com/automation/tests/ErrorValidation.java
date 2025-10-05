a9335abefd082d80516f685f5e11898cad49f59a
ajaajahmad <ajaaj75@gmail.com>
2025-10-05 16:27:48 +0530

    Page Object Model

--- src/test/java/com/automation/tests/ErrorValidation.java
+++ src/test/java/com/automation/tests/ErrorValidation.java
@@ -1,18 +1,38 @@
 package com.automation.tests;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.automation.base.BaseTest;
+import com.automation.pages.CartPage;
+import com.automation.pages.ProductCateloguePage;
 
 public class ErrorValidation extends BaseTest {
 
 	@Test
-	public void errorValidation() {
+	public void loginErrorValidation() {
 
 		String productName = "ZARA COAT 3";
-		landingPage.userLogin("test.user@domain.com", "Asdf@123");
-		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+		landingPage.userLogin("test.user@domai.com", "Asdf@123");
+		Assert.assertEquals(landingPage.getErrorMessage(), "Incorrect email or password.");
+	}
+
+	@Test
+	public void productErrorValidation() throws IOException, InterruptedException {
+
+		String productName = "ZARA COAT 3";
+		ProductCateloguePage productCatelogue = landingPage.userLogin("test.user@domain.com", "Asdf@123");
+		List<WebElement> productList = productCatelogue.getProductList();
+		productCatelogue.addProductToCard(productName);
+		CartPage cartPage = productCatelogue.goToCardPage();
+
+		boolean match = cartPage.verifyProducDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+
 	}
 
 }
